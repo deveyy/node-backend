f62b0cd37c5eed7bc495390c65a37374b7e49629
Tên của bạn <jungjihoonkr97@gmail.com>
2022-09-30 20:30:49 +0700

    global error handle

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -13,6 +13,7 @@ import 'express-async-errors';
 import applicationRoutes from './routes';
 
 import { config } from './config';
+import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
 
 const SERVER_PORT = 5000;
 
@@ -63,7 +64,18 @@ export class bdigitalServer {
         applicationRoutes(app);
     }
 
-    private globalErrorHandler(app: Application): void{}
+    private globalErrorHandler(app: Application): void{
+        app.all('*', (req: Request, res: Response) => {
+           res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found`}) 
+        });
+        app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+            console.log(error);
+            if (error instanceof CustomError) {
+                return res.status(error.statusCode).json(error.serializeErrors());
+            }
+            next();
+        })
+    }
 
     private async startServer(app: Application): Promise<void> {
         try {
