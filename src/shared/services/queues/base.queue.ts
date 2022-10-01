a1ae8736c482025eeaf6dad183bb515e6423d68e
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-01 13:12:22 +0700

    base message queue

---
+++ src/shared/services/queues/base.queue.ts
@@ -0,0 +1,45 @@
+import Queue, { Job } from 'bull';
+import Logger from 'bunyan';
+
+
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+
+import { config } from '@root/config';
+
+let bullAdapters: BullAdapter[] = [];
+export let serverAdapter: ExpressAdapter;
+
+export abstract class BaseQueue {
+  queue: Queue.Queue;
+  log: Logger;
+
+  constructor(queueName: string) {
+    this.queue = new Queue(queueName, `${config.REDIS_HOST}`);
+    bullAdapters.push(new BullAdapter(this.queue));
+    bullAdapters = [...new Set(bullAdapters)];
+    serverAdapter = new ExpressAdapter();
+    serverAdapter.setBasePath('/queues');
+
+    createBullBoard({
+      queues: bullAdapters,
+      serverAdapter
+    });
+
+    this.log = config.createLogger(`${queueName}Queue`);
+
+    this.queue.on('completed', (job: Job) => {
+      job.remove();
+    });
+
+    this.queue.on('global:completed', (jobId: string) => {
+      this.log.info(`Job ${jobId} completed`);
+    });
+
+    this.queue.on('global:stalled', (jobId: string) => {
+      this.log.info(`Job ${jobId} is stalled`);
+    });
+
+  }
+}
