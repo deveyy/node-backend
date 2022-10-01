bfb83b2988887da8294e0807422b358fe33f820c
deveyy <108226797+deveyy@users.noreply.github.com>
2022-10-01 14:34:02 +0700

    add login with jwt session (#9)
    * add jwt to session
    
    * user login controller
    
    * update
    
    Co-authored-by: @deveyy @ddthien-coder  <jungjihoonkr97@gmail.com>

---
+++ src/features/auth/controllers/signin.ts
@@ -0,0 +1,40 @@
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { loginSchema } from '@auth/schemas/signin';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { config } from '@root/config';
+import { authService } from '@service/db/auth.service';
+import { Request, Response } from 'express';
+import JWT from 'jsonwebtoken';
+import  HTTP_STATUS  from 'http-status-codes';
+
+export class SignIn {
+  @joiValidation(loginSchema)
+  public async read(req: Request, res: Response): Promise<void> {
+
+    const {username, password} = req.body;
+
+    const existingUser: IAuthDocument = await authService.getAuthUserByUsername(username);
+    if (!existingUser) {
+      throw new BadRequestError('Invalid user');
+    }
+
+    const passwordsMatch: boolean = await existingUser.comparePassword(password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid password');
+    }
+
+    const userJwt: string = JWT.sign(
+      {
+        userId: existingUser._id,
+        uId: existingUser.uId,
+        email: existingUser.email,
+        username: existingUser.username,
+        avatarColor: existingUser.avatarColor
+      },
+      config.JWT_TOKEN!
+    );
+    req.session = { jwt: userJwt };
+    res.status(HTTP_STATUS.OK).json({ message: 'User login successfully', user: existingUser, token: userJwt });
+  }
+}
--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -14,6 +14,8 @@ import { UserCache } from '@service/redis/user.cache';
 import { omit } from 'lodash';
 import { authQueue } from '@service/queues/auth.queue';
 import { userQueue } from '@service/queues/user.queue';
+import JWT from 'jsonwebtoken';
+import { config } from '@root/config';
 
 const userCache: UserCache = new UserCache();
 
@@ -22,7 +24,7 @@ export class SignUp {
   public async create(req: Request, res: Response): Promise<void> {
       const {username, email, password, avatarColor, avatarImage} = req.body;
       const checkIfUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
-    if (checkIfUserExist) {
+    if (!checkIfUserExist) {
       throw new BadRequestError('Invalid credentials');
     }
 
@@ -57,8 +59,24 @@ export class SignUp {
     authQueue.addAuthUserJob('addAuthUserToDB', { value: userDataForCache });
     userQueue.addUserJob('addUserToDB', { value: userDataForCache });
 
+    // JWT
+    const userJwt: string = SignUp.prototype.signupToken(authData, userObjectId);
+    req.session = { jwt: userJwt };
 
-    res.status(HTTP_STATUS.CREATED).json({message: 'User created successfully!', authData});
+    res.status(HTTP_STATUS.CREATED).json({message: 'User created successfully!', user: userDataForCache, token: userJwt});
+  }
+
+  private signupToken(data: IAuthDocument, userObjectId: ObjectId): string {
+    return JWT.sign(
+      {
+        userId: userObjectId,
+        uId: data.uId,
+        email: data.email,
+        username: data.username,
+        avatarColor: data.avatarColor
+      },
+      config.JWT_TOKEN!
+    );
   }
 
   private signupData(data: ISignUpData): IAuthDocument {
