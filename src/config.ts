baf09a0e9dc93d175870ee294dc86a3a9b5b8b3f
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-01 08:45:57 +0700

    cloudinary upload function

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 import bunyan from 'bunyan';
 
+import cloudinary from 'cloudinary';
+
+
 dotenv.config({});
 
 class Config {
@@ -13,8 +16,12 @@ class Config {
     public SECRET_KEY_TWO: string | undefined;
     public CLIENT_URL: string | undefined;
     public REDIS_HOST: string | undefined;
+    public CLOUD_NAME: string | undefined;
+    public CLOUD_API_KEY: string | undefined;
+    public CLOUD_API_SECRET: string | undefined;
 
-    private readonly DEFAULT_MONGO_URL = 'cluster0.ir5nru3.mongodb.net/?retryWrites=true&w=majority'
+
+    private readonly DEFAULT_MONGO_URL = 'cluster0.ir5nru3.mongodb.net/?retryWrites=true&w=majority';
 
     constructor() {
         this.MONGO_URL = process.env.MONGO_URL || this.DEFAULT_MONGO_URL;
@@ -26,6 +33,9 @@ class Config {
         this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
         this.CLIENT_URL = process.env.CLIENT_URL || '';
         this.REDIS_HOST = process.env.REDIS_HOST || '';
+        this.CLOUD_NAME = process.env.CLOUD_NAME || '';
+        this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
+        this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
     }
 
     public createLogger(name: string): bunyan {
@@ -39,6 +49,14 @@ class Config {
                 throw new Error(`Configuration ${key} is undefined`);
             }
         }
+    }
+
+    public cloudinaryConfig(): void {
+      cloudinary.v2.config({
+        cloud_name: this.CLOUD_NAME,
+        api_key: this.CLOUD_API_KEY,
+        api_secret: this.CLOUD_API_SECRET
+      });
     }
 };
 
