bec3b17b09cd6cfc20ad4f6505c320a57f5a1a87
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-01 09:06:09 +0700

    update

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -113,7 +113,7 @@ export class bdigitalServer {
     }
 
     private socketIOConnections(io: Server): void {
-      log.info('socketIOConnections');
+      log.info('socketIOConnections', io);
     }
 
 }
