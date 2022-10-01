955189f754224add4ef313bf745aa04c8dc3e707
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-01 16:58:33 +0700

    authentication middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { authRoutes } from '@auth/routes/authRoutes';
+import { currentUserRoutes } from '@auth/routes/currentRoutes';
+import { authMiddleware } from '@global/helpers/auth-middleware';
 import { serverAdapter } from '@service/queues/base.queue';
 import { Application } from 'express';
 
@@ -9,6 +11,8 @@ export default (app: Application) => {
       app.use('/queues', serverAdapter.getRouter());
       app.use(BASE_PATH, authRoutes.routes());
       app.use(BASE_PATH, authRoutes.signoutRoute());
+
+      app.use(BASE_PATH, authMiddleware.verifyUser,currentUserRoutes.routes());
     };
     routes();
 };
