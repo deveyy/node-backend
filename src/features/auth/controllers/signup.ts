2df833f24031f345c13e2193df019494f70e2b36
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-01 09:51:33 +0700

    signup user medthod - 002

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { ISignUpData } from './../interfaces/auth.interface';
 import { ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
@@ -5,6 +6,9 @@ import { signupSchema } from '@auth/schemas/signup';
 import { IAuthDocument } from '@auth/interfaces/auth.interface';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { authService } from '@service/db/auth.service';
+import { Helpers } from '@global/helpers/helpers';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
 
 export class SignUp {
   @joiValidation(signupSchema)
@@ -14,5 +18,39 @@ export class SignUp {
     if (checkIfUserExist) {
       throw new BadRequestError('Invalid credentials');
     }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+
+    // the reason we are using SignUp.prototype.signupData and not this.signupData is because
+    // of how we invoke the create method in the routes method.
+    // the scope of the this object is not kept when the method is invoked
+    const authData: IAuthDocument = SignUp.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    });
+    const result: UploadApiResponse = (await uploads(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError('File upload: Error occurred. Try again.');
+    }
   }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowerCase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as IAuthDocument;
+  }
+
 }
--- src/shared/globals/helpers/helpers.ts
+++ src/shared/globals/helpers/helpers.ts
@@ -10,4 +10,14 @@ export class Helpers {
   static lowerCase(str: string): string {
     return str.toLowerCase();
   }
+
+  static generateRandomIntegers(integerLength: number): number {
+    const characters = '0123456789';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < integerLength; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return parseInt(result, 10);
+  }
 }
