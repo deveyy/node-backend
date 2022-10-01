96ecac40db7a6bac1623dd168f723ff2c9713646
deveyy <108226797+deveyy@users.noreply.github.com>
2022-10-01 20:08:08 +0700

    feat: 007-008 config reset password (#13)
    * feat: config emails forgot and reset pass templates
    
    * feat: 006-Test email with reset template
    
    * feat: 007-008 config reset password
    
    Co-authored-by: @ddthien-coder  @deveyy  <jungjihoonkr97@gmail.com>

--- src/features/auth/routes/authRoutes.ts
+++ src/features/auth/routes/authRoutes.ts
@@ -1,4 +1,5 @@
 
+import { Password } from '@auth/controllers/password';
 import { SignIn } from '@auth/controllers/signin';
 import { SignOut } from '@auth/controllers/signout';
 import { SignUp } from '@auth/controllers/signup';
@@ -14,6 +15,9 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
     this.router.post('/signin', SignIn.prototype.read);
+    this.router.post('/forgot-password', Password.prototype.create);
+    this.router.post('/reset-password/:token', Password.prototype.update);
+
     return this.router;
   }
 
