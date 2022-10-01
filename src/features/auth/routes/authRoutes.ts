d0a70ef3b50d47d73f245bea9a3982b32373d326
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-01 14:55:41 +0700

    add user logout

--- src/features/auth/routes/authRoutes.ts
+++ src/features/auth/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 
 import { SignIn } from '@auth/controllers/signin';
+import { SignOut } from '@auth/controllers/signout';
 import { SignUp } from '@auth/controllers/signup';
 import express, { Router } from 'express';
 
@@ -13,6 +14,11 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
     this.router.post('/signin', SignIn.prototype.read);
+    return this.router;
+  }
+
+  public signoutRoute(): Router {
+    this.router.get('/signout', SignOut.prototype.update);
     return this.router;
   }
 };
--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ export default (app: Application) => {
     const routes = () => {
       app.use('/queues', serverAdapter.getRouter());
       app.use(BASE_PATH, authRoutes.routes());
+      app.use(BASE_PATH, authRoutes.signoutRoute());
     };
     routes();
 };
