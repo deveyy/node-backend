2ba26148606f2d21dc273bffe810f6f29c21b691
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-02 09:13:12 +0700

    feat: User save post to redis cache method

--- src/features/post/controllers/create-post.ts
+++ src/features/post/controllers/create-post.ts
@@ -1,16 +1,20 @@
-import { joiValidation } from '@global/decorators/joi-validation.decorators';
 import { Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
 import HTTP_STATUS from 'http-status-codes';
 import { IPostDocument } from '@post/interfaces/post.interface';
+import { PostCache } from '@service/redis/post.cache';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { postSchema } from '@post/schemas/post.schemas';
 
+const postCache: PostCache = new PostCache();
 
 export class Create {
+    @joiValidation(postSchema)
     public async post(req: Request, res: Response): Promise<void> {
       const {post, bgColor, privacy, gifUrl, profilePicture, feelings} = req.body;
 
       const postObjectId: ObjectId = new ObjectId();
-      const createPost: IPostDocument = {
+      const createdPost: IPostDocument = {
         _id: postObjectId,
         userId: req.currentUser!.userId,
         username: req.currentUser!.username,
@@ -29,6 +33,13 @@ export class Create {
         reactions: {like: 0, love: 0, sad: 0, wow: 0, angry: 0, haha: 0}
 
       } as IPostDocument;
+
+      await postCache.savePostToCache({
+        key: postObjectId,
+        currentUserId: `${req.currentUser!.userId}`,
+        uId: `${req.currentUser!.uId}`,
+        createdPost
+      });
 
       res.status(HTTP_STATUS.CREATED).json({ message: 'Post created successfully'});
     }
