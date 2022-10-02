9ee57a17fd3022c3980a66955343e790c3029789
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-02 15:44:31 +0700

    remove post reaction from redis cache

--- src/shared/services/redis/reaction.cache.ts
+++ src/shared/services/redis/reaction.cache.ts
@@ -3,6 +3,8 @@ import Logger from 'bunyan';
 import { config } from '@root/config';
 import { IReactionDocument, IReactions } from '@reaction/interfaces/reaction.interface';
 import { ServerError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
+import { find } from 'lodash';
 
 const log: Logger = config.createLogger('reactionsCache');
 
@@ -25,6 +27,7 @@ export class ReactionCache extends BaseCache {
 
       if (previousReaction) {
         // call remove reaction method
+        this.removePostReactionFromCache(key, reaction.username, postReactions);
       }
 
       if (type) {
@@ -36,5 +39,34 @@ export class ReactionCache extends BaseCache {
       log.error(error);
       throw new ServerError('Server error. Try again.');
     }
+  }
+
+  public async removePostReactionFromCache(key: string, username: string, postReactions: IReactions): Promise<void> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const response: string[] = await this.client.LRANGE(`reactions:${key}`, 0, -1);
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      const userPreviousReaction: IReactionDocument = this.getPreviousReaction(response, username) as IReactionDocument;
+      multi.LREM(`reactions:${key}`, 1, JSON.stringify(userPreviousReaction));
+      await multi.exec();
+
+      const dataToSave: string[] = ['reactions', JSON.stringify(postReactions)];
+      await this.client.HSET(`posts:${key}`, dataToSave);
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  private getPreviousReaction(response: string[], username: string): IReactionDocument | undefined {
+    const list: IReactionDocument[] = [];
+    for (const item of response) {
+      list.push(Helpers.parseJson(item) as IReactionDocument);
+    }
+    return find(list, (listItem: IReactionDocument) => {
+      return listItem.username === username;
+    });
   }
 }
