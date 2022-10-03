357241355be86cedfd2fd27250609b0bc0ef0a94
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-03 20:24:24 +0700

    add profile and background image

--- src/shared/globals/helpers/helpers.ts
+++ src/shared/globals/helpers/helpers.ts
@@ -30,4 +30,21 @@ export class Helpers {
     }
     return JSON.parse(prop);
   }
+
+  static isDataURL(value: string): boolean {
+    const dataUrlRegex = /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\\/?%\s]*)\s*$/i;
+    return dataUrlRegex.test(value);
+  }
+
+  static shuffle(list: string[]): string[] {
+    for (let i = list.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [list[i], list[j]] = [list[j], list[i]];
+    }
+    return list;
+  }
+
+  static escapeRegex(text: string): string {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
 }
