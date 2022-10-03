d98aa68555ce0269d2d80ef4d5a2ea0eda8c3989
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-03 20:13:11 +0700

    get images from mongodb

--- src/shared/services/queues/base.queue.ts
+++ src/shared/services/queues/base.queue.ts
@@ -10,8 +10,10 @@ import { IPostJobData } from '@post/interfaces/post.interface';
 import { IReactionJob } from '@reaction/interfaces/reaction.interface';
 import { ICommentJob } from '@comment/interfaces/comment.interface';
 import { IBlockedUserJobData, IFollowerJobData } from '@follower/interfaces/follower.interface';
+import { IFileImageJobData } from '@image/interfaces/image.interface';
+import { INotificationJobData } from '@notification/interfaces/notification.interface';
 
-type IBaseJobData = IAuthJob | IEmailJob | IPostJobData | IReactionJob | ICommentJob | IFollowerJobData | IBlockedUserJobData;
+type IBaseJobData = IAuthJob | IEmailJob | IPostJobData | IReactionJob | ICommentJob | IFollowerJobData | IBlockedUserJobData | INotificationJobData | IFileImageJobData;
 
 let bullAdapters: BullAdapter[] = [];
 export let serverAdapter: ExpressAdapter;
