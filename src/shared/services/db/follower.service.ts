fdab8c9b2dc9c5d80c15ca0e7ba83639d960761e
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-03 10:08:18 +0700

    get Follower from mongodb

--- src/shared/services/db/follower.service.ts
+++ src/shared/services/db/follower.service.ts
@@ -3,7 +3,7 @@ import { UserModel } from '@user/models/user.schema';
 import { ObjectId, BulkWriteResult } from 'mongodb';
 import mongoose, { Query } from 'mongoose';
 import { IQueryDeleted, IQueryComplete } from '@post/interfaces/post.interface';
-import { IFollowerDocument } from '@follower/interfaces/follower.interface';
+import { IFollowerData, IFollowerDocument } from '@follower/interfaces/follower.interface';
 class FollowerService {
 
   public async addFollowerToDB(userId: string, followeeId: string, username: string, followerDocumentId: ObjectId): Promise<void> {
@@ -61,6 +61,71 @@ class FollowerService {
     await Promise.all([unfollow, users]);
   }
 
+  public async getFolloweeData(userObjectId: ObjectId): Promise<IFollowerData[]> {
+    const followee: IFollowerData[] = await FollowerModel.aggregate([
+      { $match: { followerId: userObjectId } },
+      { $lookup: { from: 'User', localField: 'followeeId', foreignField: '_id', as: 'followeeId' } },
+      { $unwind: '$followeeId' },
+      { $lookup: { from: 'Auth', localField: 'followeeId.authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      {
+        $addFields: {
+          _id: '$followeeId._id',
+          username: '$authId.username',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          postCount: '$followeeId.postsCount',
+          followersCount: '$followeeId.followersCount',
+          followingCount: '$followeeId.followingCount',
+          profilePicture: '$followeeId.profilePicture',
+          userProfile: '$followeeId'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          followerId: 0,
+          followeeId: 0,
+          createdAt: 0,
+          __v: 0
+        }
+      }
+    ]);
+    return followee;
+  }
+
+  public async getFollowerData(userObjectId: ObjectId): Promise<IFollowerData[]> {
+    const follower: IFollowerData[] = await FollowerModel.aggregate([
+      { $match: { followeeId: userObjectId } },
+      { $lookup: { from: 'User', localField: 'followerId', foreignField: '_id', as: 'followerId' } },
+      { $unwind: '$followerId' },
+      { $lookup: { from: 'Auth', localField: 'followerId.authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      {
+        $addFields: {
+          _id: '$followerId._id',
+          username: '$authId.username',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          postCount: '$followerId.postsCount',
+          followersCount: '$followerId.followersCount',
+          followingCount: '$followerId.followingCount',
+          profilePicture: '$followerId.profilePicture',
+          userProfile: '$followerId'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          followerId: 0,
+          followeeId: 0,
+          createdAt: 0,
+          __v: 0
+        }
+      }
+    ]);
+    return follower;
+  }
 }
 
 export const followerService: FollowerService = new FollowerService();
