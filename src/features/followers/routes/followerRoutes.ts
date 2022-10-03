a61d48510d1fd1a275a7666eeff2991de8a26aba
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-03 10:20:30 +0700

    get-follower controller and api

--- src/features/followers/routes/followerRoutes.ts
+++ src/features/followers/routes/followerRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@follower/controllers/follower-user';
 import { Remove } from '@follower/controllers/unfollow-user';
+import { Get } from '@follower/controllers/get-followers';
 
 class FollowerRoutes {
   private router: Router;
@@ -11,6 +12,9 @@ class FollowerRoutes {
   }
 
   public routes(): Router {
+
+    this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
+    this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
 
     this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
     this.router.put('/user/unfollow/:followerId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
