9c513a336874db27e3cc12d0e484104889aa0388
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-03 20:47:47 +0700

    delete image api

--- src/features/images/routes/imageRoutes.ts
+++ src/features/images/routes/imageRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@image/controllers/add-image';
+import { Delete } from '@image/controllers/delete-image';
 
 class ImageRoutes {
   private router: Router;
@@ -13,6 +14,9 @@ class ImageRoutes {
 
     this.router.post('/images/profile', authMiddleware.checkAuthentication, Add.prototype.profileImage);
     this.router.post('/images/background', authMiddleware.checkAuthentication, Add.prototype.backgroundImage);
+
+    this.router.delete('/images/:imageId', authMiddleware.checkAuthentication, Delete.prototype.image);
+    this.router.delete('/images/background/:bgImageId', authMiddleware.checkAuthentication, Delete.prototype.backgroundImage);
 
     return this.router;
   }
