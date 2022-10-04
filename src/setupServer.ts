26f69bf284c15d7976aa3bcd39bffa0f38e0550c
deveyy <108226797+deveyy@users.noreply.github.com>
2022-10-04 08:44:28 +0700

    feat: 024-implement chat feature (#24)
    * chat interface, model, schema and joi
    
    * add chat controolers
    
    * add chat list to redis cache
    
    * add chat message route
    
    * add chat message to redis cache
    
    * add chat users to redis cache
    
    * add chat message to mongodb
    
    * get conversation list from redis cache
    
    * get chat messages
    
    * mark messages as deleted in cache
    
    * mark messages as delete in mongodb
    
    * Update messages in redis cache
    
    * update messages in mongodb
    
    * fix bug messages update in redis cache
    
    * add messages reaction to redis cache
    
    * add messages reaction to mongodb
    
    * add messages unitTest
    
    Co-authored-by: @deveyy

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -19,6 +19,7 @@ import { SocketIOFollowerHandler } from '@socket/follower';
 import { SocketIOUserHandler } from '@socket/user';
 import { SocketIONotificationHandler } from '@socket/notification';
 import { SocketIOImageHandler } from '@socket/image';
+import { SocketIOChatHandler } from '@socket/chat';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -122,12 +123,13 @@ export class BdigitalServer {
     const userSocketHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
     const notificationSocketHandler: SocketIONotificationHandler = new SocketIONotificationHandler();
     const imageSocketHandler: SocketIOImageHandler = new SocketIOImageHandler();
-
+    const chatSocketHandler: SocketIOChatHandler = new SocketIOChatHandler(io);
 
     postSocketHandler.listen();
     followerSocketHandler.listen();
     userSocketHandler.listen();
     notificationSocketHandler.listen(io);
     imageSocketHandler.listen(io);
+    chatSocketHandler.listen();
   }
 }
--- src/shared/sockets/user.ts
+++ src/shared/sockets/user.ts
@@ -2,6 +2,7 @@ import { ISocketData } from '@user/interfaces/user.interface';
 import { Server, Socket } from 'socket.io';
 
 export let socketIOUserObject: Server;
+export const connectedUsersMap: Map<string, string> = new Map();
 
 export class SocketIOUserHandler {
   private io: Server;
